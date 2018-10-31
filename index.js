3939f6c5bec24aa5ede9c715e085431befc70154
melanie jaramillo <melaniejaramillo87@gmail.com>
2018-10-31 03:27:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function logWhisper(string) {
   console.log(whisper(string))
 }
 function sayHiToGrandma(string){
-  if (whisper(string)) {return "I can\'t hear you!"
+  if (whisper(string)) {
+    return "I can\'t hear you!"
 } else if (shout(string)) {
   return "YES INDEED!"
   }
